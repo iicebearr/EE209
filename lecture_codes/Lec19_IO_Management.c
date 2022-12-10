0e41b68658a3640e6d1b08d95cdfa39e0ca845ed
iicebearr <eungjoekang@gmail.com>
2022-12-10 12:29:03 +0900

    new file:   lecture_codes/Lec16_Exceptions 	new file:   lecture_codes/Lec19_IO_Management 	new file:   lecture_codes/Lec19_IO_Management.c 	new file:   lecture_codes/mytext.txt 	deleted:    lecture_codes/tempCodeRunnerFile.c

---
+++ lecture_codes/Lec19_IO_Management.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+// stream: any source of input or destination for output
+// accessed in c programs through file pointers
+
+// three streams are provided by stdio.h
+// stdin, stdout, stderr
+
+// standard I/O functions wrap system calls
+// standard I/O functions provide additional functionalities such as buffered I/O
+
+// file descriptor: an integer that uniquely identifies an open file
+
+// standard I/O functions use file pointers that points to FILE structure
+// system calls use file descripters
+// file descripters are within the FILE structure
+
+int main(void)
+{
+    /* standard I/O functions */ 
+    int i = 1;
+    char text[100];
+
+    FILE *fp1 = fopen("mytext.txt", "w");
+    fprintf(fp1, "Number:%d\n", i);
+    fflush(fp1);
+
+    FILE *fp2 = fopen("mytext.txt", "r");
+    fscanf(fp2, "%s", text);
+    fprintf(stdout, "mytext.txt: %s\n", text);
+
+    fclose(fp1);
+    fclose(fp2);
+
+
+    /* system calls for I/O */
+    // int creat(char *pathname, mode_t mode);
+    //      Creates a new file named pathname, and returns a file descriptor
+    
+    // int open(char *pathname, int flags, mode_t mode);
+    //      Opens the file pathname and returns a file descriptor
+    
+    // int close(int fd);
+    //      Closes fd
+    
+    // int read(int fd, void *buf, int count);
+    //      Reads up to count bytes from fd into the buffer at buf
+    //      Returns the number of bytes read
+    
+    // int write(int fd, void *buf, int count);
+    //      Writes up to count bytes into fd from the buffer at buf
+    
+    // int lseek(int fd, int offset, int whence);
+    //      Assigns the file pointer of fd to a new value by applying an offset
+
+
+    /* bufferd I/O */
+    printf("Step 1\n");
+    fflush(stdout); // flush the output buffer
+    sleep(3);
+    printf("Step 2\n");
+
+
+
+}
